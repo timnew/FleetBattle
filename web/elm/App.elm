515a4464f807dba2029bfbca4c4ebddf4c32e2fd
DD <timnew.wti@gmail.com>
2017-04-22 21:57:28 +1000

    Setup the basic structure of elm

--- web/elm/App.elm
+++ web/elm/App.elm
@@ -3,8 +3,24 @@ module App exposing (main)
 import Html exposing (..)
 
 
+type alias Player =
+    String
+
+
+type Cell
+    = Blank
+    | Ship Player
+    | Damaged Player
+
+
+type alias Grid =
+    List Cell
+
+
 type alias Model =
-    {}
+    { players : ( Player, Player )
+    , grids : ( Grid, Grid )
+    }
 
 
 type Msg
@@ -13,10 +29,9 @@ type Msg
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model, Cmd.none )
-
-
-import Types exposing (..)
+    ( Model ( "P1", "P2" ) ( List.repeat 8 Blank, List.repeat 8 Blank )
+    , Cmd.none
+    )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -29,17 +44,31 @@ subscriptions model =
     Sub.none
 
 
+grid : Grid -> Html Msg
+grid gridData =
+    text "Grid"
 
-view : Model -> Html Msg
-view model =
-    text "Bare Cool"
+
+root : Model -> Html Msg
+root { players, grids } =
+    let
+        ( player1, player2 ) =
+            players
+
+        ( grid1, grid2 ) =
+            grids
+    in
+        div []
+            [ grid grid1
+            , grid grid2
+            ]
 
 
 main : Program Never Model Msg
 main =
     Html.program
         { init = init
-        , view = view
+        , view = root
         , update = update
         , subscriptions = subscriptions
         }
